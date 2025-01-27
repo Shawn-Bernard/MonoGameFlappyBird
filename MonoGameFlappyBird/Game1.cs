94ce4599af9823a70aee3e8df978319d738c4c2b
Shawn Bernard <ssbernard@outlook.com>
2025-01-27 12:40:45 -0400

    Added player png & fixed file

--- MonoGameFlappyBird/Game1.cs
+++ MonoGameFlappyBird/Game1.cs
@@ -68,7 +68,7 @@ namespace FlappyBird
                 isJumpPressed = false;
             }
 
-            playerPosition.Y += Gravity * deltaTime;
+            playerPosition.Y += Gravity ;
 
             if (Gravity < maxGravity)
             {
--- MonoGameFlappyBird/Program.cs
+++ MonoGameFlappyBird/Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new MonoGameFlappyBird.Game1();
+using var game = new FlappyBird.Game1();
 game.Run();
